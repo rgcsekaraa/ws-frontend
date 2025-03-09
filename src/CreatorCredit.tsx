cdb6eb0c9ffea7ef963a187b4dd178255e650d12
Chandrasekaraa Ramadoss Ganesh <94929823+RGChandrasekaraa@users.noreply.github.com>
2025-03-09 13:13:39 +1000

    onclick

--- src/CreatorCredit.tsx
+++ src/CreatorCredit.tsx
@@ -2,15 +2,16 @@ const CreatorCredit = () => {
   return (
     <div className="creator-credit">
       <div className="creator-info">
-        <p>
-          Created by <span className="creator-name">Chan RG</span>
-        </p>
         <a
-          href="https://www.sekaraa.com"
+          href="https://www.linkedin.com/in/rgcsekaraa/"
           target="_blank"
           rel="noopener noreferrer"
           className="creator-link"
-        ></a>
+        >
+          <p>
+            Created by <span className="creator-name">Chan RG</span>
+          </p>
+        </a>
       </div>
     </div>
   );
